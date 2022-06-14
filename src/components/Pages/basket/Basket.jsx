5c78887192118be04b618803e57c086dc5546d1d
Vlad <vladonish@gmail.com>
2022-06-14 21:00:47 +0300

    Navigate

--- src/components/Pages/basket/Basket.jsx
+++ src/components/Pages/basket/Basket.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './basket.scss'
+import Button from "../../button/Button";
+import {Link, useNavigate} from "react-router-dom";
 
-const Basket = () => {
+const Basket = (props) => {
+  const navigate = useNavigate()
+  const goBack = () => navigate(-1)
+  const goHome = () => navigate('/', {replace: true})
+
   return (
     <section className="store-items">
+      <Button styles="goBack-btn" handlerClick={goBack} text="Go Back"/>
+      <Button styles="goHome-btn" handlerClick={goHome} text="Go Home"/>
       <h1>Basket</h1>
     </section>
   );
--- src/components/Pages/favourites/Favourites.jsx
+++ src/components/Pages/favourites/Favourites.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './favourites.scss'
+import Button from "../../button/Button";
+import {Link, useNavigate} from "react-router-dom";
 
 const Favourites = () => {
+  const navigate = useNavigate()
+
+  const goBack = () => navigate(-1)
+  const goHome = () => navigate('/', {replace: true})
+
   return (
     <section className="store-items">
+      <Button styles="goBack-btn" handlerClick={goBack} text="Go Back"/>
+      <Button styles="goHome-btn" handlerClick={goHome} text="Go Home"/>
         <h1>Favourites</h1>
     </section>
   );
