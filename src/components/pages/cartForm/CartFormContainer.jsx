b2075c587da3655a8566fec09eb4416dd7bf74f4
Vlad <vladonish@gmail.com>
2022-07-09 21:10:27 +0300

    Validation Styles

--- src/components/pages/cartForm/CartFormContainer.jsx
+++ src/components/pages/cartForm/CartFormContainer.jsx
@@ -7,6 +7,7 @@ import {useSelector} from "react-redux";
 const CartFormContainer = () => {
   const { cartReducer } = useSelector(state => state)
   const { cart } = cartReducer
+  const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
 
   const initialValues = {
     name: '',
@@ -16,11 +17,22 @@ const CartFormContainer = () => {
     phoneNumber: ''
   }
   const validationSchema = Yup.object({
-    name: Yup.string().min(3, 'Must be more than 3 characters').required("Name is required"),
-    surname: Yup.string().min(5, 'Must be more than 5 characters').required("Surname is required"),
-    userAge: Yup.number().required("Age is required"),
-    address: Yup.string().required("Address is required"),
-    phoneNumber: Yup.string().required("Phone is required"),
+    name: Yup.string()
+      .min(3, 'Must be more than 3 characters')
+      .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field ")
+      .required("Name is required"),
+    surname: Yup.string()
+      .min(5, 'Must be more than 5 characters')
+      .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field ")
+      .required("Surname is required"),
+    userAge: Yup.number()
+      .typeError('You must specify a number')
+      .required("Age is required"),
+    address: Yup.string()
+      .required("Address is required"),
+    phoneNumber: Yup.string()
+      .matches(phoneRegExp, 'Phone number is not valid')
+      .required("Phone is required"),
   })
 
   const onSubmit = (values) => console.log("Submit form", values)
@@ -50,8 +62,8 @@ const CartFormContainer = () => {
             <FormControl control="text" name="name" label="Name"/>
             <FormControl control="text" name="surname" label="Surname"/>
             <FormControl control="text" name="userAge" label="Age"/>
-            <FormControl control="text" name="address" label="Address"/>
             <FormControl control="text" name="phoneNumber" label="Phone number"/>
+            <FormControl control="text" name="address" label="Address"/>
             <h4>Total price: ${priceCounter()}</h4>
             <button
               type="submit"
